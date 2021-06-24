2221effff22c29f28756143bd48cdf58a8b4eca5
Anarak404 <annaarak2@gmail.com>
2021-06-25 01:51:08 +0200

    feat(components): add post button connected with API

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -1,13 +1,16 @@
 import { Box, Fab, makeStyles } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import React from 'react';
+import { useCallback } from 'react';
+import { useState } from 'react';
 import { IPost } from '../types';
+import PostEditor from './PostEditor';
 import PostItem from './PostItem';
 
 interface IProps {
   canModify: boolean;
   data: IPost[];
-  showPost(id: number): void
+  showPost(id: number): void;
 }
 
 const useStyles = makeStyles({
@@ -21,16 +24,32 @@ const useStyles = makeStyles({
 export default function PostList({ canModify, data, showPost }: IProps) {
   const classes = useStyles();
 
+  const [open, setOpen] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setOpen((s) => !s);
+  }, [setOpen]);
+
   return (
     <Box>
       {data.map((e) => (
-        <PostItem {...e} canModify={canModify} showPost={() => showPost(e.id)} />
+        <PostItem
+          {...e}
+          canModify={canModify}
+          showPost={() => showPost(e.id)}
+        />
       ))}
       {canModify && (
-        <Fab color="primary" aria-label="add" className={classes.addButton}>
+        <Fab
+          color="primary"
+          aria-label="add"
+          className={classes.addButton}
+          onClick={handleClick}
+        >
           <AddIcon />
         </Fab>
       )}
+      {open && <PostEditor />}
     </Box>
   );
 }
