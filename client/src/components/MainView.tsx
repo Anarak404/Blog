def439260f18e470f76400a34e9bee95ff55fede
Anarak404 <annaarak2@gmail.com>
2021-06-25 03:50:14 +0200

    feat(components): get user with roles to admin page

--- client/src/components/MainView.tsx
+++ client/src/components/MainView.tsx
@@ -10,8 +10,9 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import React from 'react';
 import { useState } from 'react';
 import { useCallback } from 'react';
+import { useEffect } from 'react';
 import { useContext } from 'react';
-import { mainContext } from '../MainContext';
+import { mainContext, url } from '../MainContext';
 import { IUser } from '../types';
 import Post from './Post';
 import PostList from './PostList';
@@ -38,43 +39,59 @@ const useStyles = makeStyles({
   },
 });
 
-const userRoles: IUser[] = [
-  {
-    id: 1,
-    name: 'name1',
-    role: 'ADMIN',
-  },
-  {
-    id: 2,
-    name: 'name2',
-    role: 'MODERATOR',
-  },
-  {
-    id: 3,
-    name: 'name3',
-    role: 'USER',
-  },
-  {
-    id: 4,
-    name: 'name4',
-    role: 'USER',
-  },
-  {
-    id: 5,
-    name: 'name5',
-    role: 'MODERATOR',
-  },
-];
+// const userRoles: IUser[] = [
+//   {
+//     id: 1,
+//     name: 'name1',
+//     role: 'ADMIN',
+//   },
+//   {
+//     id: 2,
+//     name: 'name2',
+//     role: 'MODERATOR',
+//   },
+//   {
+//     id: 3,
+//     name: 'name3',
+//     role: 'USER',
+//   },
+//   {
+//     id: 4,
+//     name: 'name4',
+//     role: 'USER',
+//   },
+//   {
+//     id: 5,
+//     name: 'name5',
+//     role: 'MODERATOR',
+//   },
+// ];
 
 type Route = 'Post' | 'Roles' | 'Posts';
 
 export default function MainView() {
   const classes = useStyles();
   const { role, posts } = useContext(mainContext);
+  const { getHeaders } = useContext(mainContext);
+
   const [id, setId] = useState(0);
+  const [data, setData] = useState<IUser[]>([]);
   const [route, setRoute] = useState<Route>('Posts');
 
   const canModify = ['ADMIN', 'MODERATOR'].includes(role);
+
+  useEffect(() => {
+    const headers = getHeaders();
+    fetch(`${url}/user/admin`, { method: 'GET', headers })
+      .then(async (response) => {
+        if (response.ok) {
+          const data: IUser[] = await response.json();
+          console.log(data);
+          setData(data);
+        }
+      })
+      .catch((e) => console.log('Error in get post', e));
+  }, [getHeaders, setData]);
 
   const showPost = useCallback(
     (id: number) => {
@@ -121,7 +138,7 @@ export default function MainView() {
           <PostList data={posts} canModify={canModify} showPost={showPost} />
         )}
         {route === 'Post' && <Post id={id} />}
-        {route === 'Roles' && <TableRoles users={userRoles} />}
+        {route === 'Roles' && <TableRoles users={data} />}
       </Container>
     </>
   );
